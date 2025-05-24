a34561040b7587282bfd2ac7206909fc5a140203
hafsahabib8 <hafsahabib870@gmail.com>
2025-05-24 21:49:48 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Home from "./components/home/home";
 import Login from "./components/login/Login";
-
+import RoleSelection from "./components/Signup/RoleSelection";
+import Signup from "./components/Signup/Signup";
+import Dashboard from "./components/dashboard/Dashboard";
+import UploadThroughPacs from "./components/upload/UploadThroughPacs";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/roleselect" element={<RoleSelection />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/upload-pacs" element={<UploadThroughPacs />} />
+
         <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
