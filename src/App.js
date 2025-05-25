c030c04708219bdcd420204ccdb345145f9e3a99
hafsahabib8 <hafsahabib870@gmail.com>
2025-05-25 12:18:47 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import LandingPage from "./components/LandingPage/LandingPage";
 import Home from "./components/home/home";
 import Login from "./components/login/Login";
 import RoleSelection from "./components/Signup/RoleSelection";
-import ResetPasswordForm from './components/ResetPasswordForm';
 import Signup from "./components/Signup/Signup";
 import Dashboard from "./components/dashboard/Dashboard";
 import UploadThroughPacs from "./components/upload/UploadThroughPacs";
+import DeveloperDashboard from "./components/DeveloperDashboard/DevelopeDashboard";
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/reset-password" element={<ResetPasswordForm />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/roleselect" element={<RoleSelection />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/upload-pacs" element={<UploadThroughPacs />} />
-
+        <Route path="/developer-dashboard" element={<DeveloperDashboard />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
